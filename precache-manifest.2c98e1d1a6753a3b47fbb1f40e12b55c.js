fe2e11eb170341ff22ac522b5b91e2f0136d258d
andicuevasp <andicuevasp@gmail.com>
2021-09-09 20:03:55 +0200

    Updates

---
+++ precache-manifest.2c98e1d1a6753a3b47fbb1f40e12b55c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "05af405bd2ff27e2bc76",
+    "url": "/mock-hotel-booking-ui/static/css/main.4134a6b5.chunk.css"
+  },
+  {
+    "revision": "05af405bd2ff27e2bc76",
+    "url": "/mock-hotel-booking-ui/static/js/main.26ff2511.chunk.js"
+  },
+  {
+    "revision": "3d43b9d0edb74d50dfb0",
+    "url": "/mock-hotel-booking-ui/static/js/runtime~main.94db46da.js"
+  },
+  {
+    "revision": "a68ee97f505489edb6f3",
+    "url": "/mock-hotel-booking-ui/static/js/2.f65867d9.chunk.js"
+  },
+  {
+    "revision": "296a051789e51477767cd6be6c6d49c7",
+    "url": "/mock-hotel-booking-ui/index.html"
+  }
+];
---
+++ service-worker.js
@@ -0,0 +1,34 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+
+importScripts(
+  "/mock-hotel-booking-ui/precache-manifest.2c98e1d1a6753a3b47fbb1f40e12b55c.js"
+);
+
+workbox.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.suppressWarnings();
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute("/mock-hotel-booking-ui/index.html", {
+  
+  blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
+});
